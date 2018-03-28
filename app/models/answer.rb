96ec3b13a982b26034423365f6b5f38ed9acf647
wilmer142 <wilmercollege@gmail.com>
2018-03-27 19:09:21 -0500

    Creating model, controller and polymorphic association for Comments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,6 +3,7 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
   has_many :votes, as: :voteable
+  has_many :comments, as: :commentable
 
   validates :content, presence: true
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,7 @@
 class Vote < ApplicationRecord
 
   belongs_to :voteable, polymorphic: true
+  belongs_to :user
 
   validates :voteable_type, :voteable_id, presence: true
 
